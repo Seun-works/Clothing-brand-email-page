75c455f5aefd5e3fec8c3cd65ef4b6845744162a
SeunOgundipe <Sogundipe@gmail.com>
2021-09-07 15:38:34 +0100

    Added the javascript file for the email validation with some adjustments

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+  // define the variables used in the function 
+
+  let email = document.querySelector('.email-input');
+  let errors = document.querySelectorAll('.error');
+
+// This is the function to validate the email.
+const validateEmail = () => {
+    if (!email.value.includes('@')) {
+        email.style.borderColor = 'hsl(0, 93%, 68%)';
+        errors.forEach(error => error.classList.add('show'));
+        setTimeout(() => errors.forEach(error => error.classList.remove('show')), 3000);
+    }
+    else {
+        email.style.borderColor = 'hsl(0, 36%, 70%)';
+        errors.forEach(error => error.classList.remove('show'));
+        window.location.reload();
+    }
+};
